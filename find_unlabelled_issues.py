4be47b3b057cddaea36bfd0d56279d8c56cc60b9
Kamil Domański <kamil@domanski.co>
2019-11-28 13:15:27 +0100

    find_unlabelled_issues.py: support slack webhook

--- find_unlabelled_issues.py
+++ find_unlabelled_issues.py
@@ -3,7 +3,9 @@
 import github
 
 import argparse
+import json
 import os
+import requests
 import sys
 
 if not 'GITHUB_TOKEN' in os.environ.keys():
@@ -14,6 +16,7 @@ g = github.Github(os.environ['GITHUB_TOKEN'])
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--repos", help="repos to check", nargs='+')
 parser.add_argument("-l", "--labels", help="labels to check", nargs='+')
+parser.add_argument("-w", "--slack_webhook")
 args = parser.parse_args()
 
 if args.repos == None:
@@ -30,13 +33,23 @@ def hasAnyOfTheLabels(issue: github.Issue.Issue, expected_labels: [str]):
             return True
     return False
 
+output = ''
+
 for reposlug in args.repos:
     unlabelled_issues = []
     for issue in g.get_repo(reposlug).get_issues():
         if issue.state == 'open' and not hasAnyOfTheLabels(issue, args.labels):
             unlabelled_issues.append(issue)
     if len(unlabelled_issues) > 0:
-        print("Repo %s:" % reposlug)
+        output += "Repo %s:\n" % reposlug
         for i in unlabelled_issues:
-            print("\thttps://github.com/%s/issues/%d %s" % (reposlug, i.number, i.title))
-        print("")
+            output += "\thttps://github.com/%s/issues/%d %s\n" % (reposlug, i.number, i.title)
+        output += '\n'
+
+if args.slack_webhook == None:
+    print(output)
+else:
+    output = ("The following issues are missing %s labels:\n\n" % args.labels) + output
+    payload = json.dumps({'channel': '@kamil', 'text': output})
+    r = requests.post(url = args.slack_webhook, data = payload)
+    print(r)
